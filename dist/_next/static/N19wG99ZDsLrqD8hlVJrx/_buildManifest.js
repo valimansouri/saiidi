55a0e27e245036db07445a7cd6b744afd6cf05e6
vali mansouri <87234921+valimansouri@users.noreply.github.com>
2024-03-08 16:16:41 -0800

    add build to project

---
+++ dist/_next/static/N19wG99ZDsLrqD8hlVJrx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/40e6793d716fccbf.css","static/chunks/pages/index-eeccdabe08a6b3e1.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
